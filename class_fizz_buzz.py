f710a569a7fe8c91d498ce2b52f3af35fafb2c91
Anabel <anabel.jesus@grupotrebolenergia.es>
2022-02-25 13:58:59 +0100

    FizzBuzz

--- class_fizz_buzz.py
+++ class_fizz_buzz.py
@@ -1,7 +1,13 @@
 # ! ANABEL
 class ClassFizzBuzz():
-    def __init__(self):
-        pass
+    def __init__(self,input):
+        self.input = input
 
-    def transform(self,input):
-        return str(input)
+    def transform(self):
+        if self.input % 3 == 0 and self.input % 5 == 0:
+            return 'FizzBuzz'
+        if self.input % 3 == 0:
+            return 'Fizz'
+        if self.input % 5 == 0:
+            return 'Buzz'
+        return str(self.input)
--- main.py
+++ main.py
@@ -5,18 +5,74 @@ from class_fizz_buzz import ClassFizzBuzz
 
 
 class ClassFizzBuzzShould(unittest.TestCase):
-    def test_returns_string_number_when_not_multiple(self):
-        fizz_buzz = ClassFizzBuzz()
-
+    def test_returns_1_as_string_when_not_multiple(self):
         #Arrange
         input = 1
-
         #Act
-        output = fizz_buzz.transform(input=input)
-
+        output = ClassFizzBuzz(input=input).transform()
         #Assert
         expected = '1'
         self.assertEqual(output,expected)
 
-    def test_multiple_of_three_when_(self):
-        pass
+    def test_returns_2_as_string_when_not_multiple(self):
+        #Arrange
+        input = 2
+        #Act
+        output = ClassFizzBuzz(input=input).transform()
+        #Assert
+        expected = '2'
+        self.assertEqual(output,expected)
+
+    def test_returns_4_as_string_when_not_multiple(self):
+        # Arrange
+        input = 4
+        # Act
+        output = ClassFizzBuzz(input=input).transform()
+        # Assert
+        expected = '4'
+        self.assertEqual(output, expected)
+
+    def test_returns_3_as_Fizz_when_multiple_of_three(self):
+        # Arrange
+        input = 3
+        # Act
+        output = ClassFizzBuzz(input=input).transform()
+        # Assert
+        expected = 'Fizz'
+        self.assertEqual(output, expected)
+
+    def test_returns_6_as_Fizz_when_multiple_of_three(self):
+        # Arrange
+        input = 6
+        # Act
+        output = ClassFizzBuzz(input=input).transform()
+        # Assert
+        expected = 'Fizz'
+        self.assertEqual(output, expected)
+
+    def test_returns_9_as_Fizz_when_multiple_of_three(self):
+        # Arrange
+        input = 9
+        # Act
+        output = ClassFizzBuzz(input=input).transform()
+        # Assert
+        expected = 'Fizz'
+        self.assertEqual(output, expected)
+
+    def test_returns_5_as_Fizz_when_multiple_of_five(self):
+        # Arrange
+        input = 5
+        # Act
+        output = ClassFizzBuzz(input=input).transform()
+        # Assert
+        expected = 'Buzz'
+        self.assertEqual(output, expected)
+
+    def test_returns_5_as_Fizz_when_multiple_of_five(self):
+        # Arrange
+        input = 15
+        # Act
+        output = ClassFizzBuzz(input=input).transform()
+        # Assert
+        expected = 'FizzBuzz'
+        self.assertEqual(output, expected)
